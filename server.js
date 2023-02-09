eb80cd6492f418c04faf19df681217dc86c15f46
fireCulex <5dillah01@gmail.com>
2023-02-09 12:58:59 +0700

    new cors origin

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors({
-  origin: ["http://localhost:3000","https://stock-mana-app-v2.vercel.app/"],
+  origin: ["https://stock-mana-app-v2.vercel.app/"],
   credentials:true
 }))
 
